8161cb1fcb336335c353e7d7bacc224dd0a18d88
Kauã Valim <kaua_a.val@outlook.com>
2024-06-17 15:34:10 -0300

    Desenvolvimento 17/06/2024
    Ajuste em arquivo PHP que não possuía bloco de código PHP

--- index.php
+++ index.php
@@ -11,38 +11,39 @@
     <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Domine:wght@400..700&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
 </head>
 
-<body>
-    <div class="listra1"></div>
-    <div class="listra2"></div>
-    <header>
-        <h1><a href="./index.php">Projetos PHP</a></h1>
-    </header>
-    <main class="maingeral">
-        <h1>Lista de Projetos</h1>
-        <ul>
-            <a href="./rifa.php">
-                <li>Rifa Online</li>
-            </a>
-            <a href="./calculadoraimc.php">
-                <li>Calculadora IMC</li>
-            </a>
-            <a href="./conversormonetario.php">
-                <li>Conversor de moedas</li>
-            </a>
-            <a href="./gorjetacalculator.php">
-                <li>Calculadora de gorjetas</li>
-            </a>
-            <a href="./tempconverter.php">
-                <li>Conversor de temperatura</li>
-            </a>
-            <a href="./calculadoraarea.php">
-                <li>Calculadora de áreas</li>
-            </a>
-        </ul>
-    </main>
-    <footer>
-        <p>Desenvolvido por Kauã Valim - 2024</p>
-    </footer>
-</body>
+<?php echo '<body>
+        <div class="listra1"></div>
+        <div class="listra2"></div>
+        <header>
+            <h1><a href="./index.php">Projetos PHP</a></h1>
+        </header>
+        <main class="maingeral">
+            <h1>Lista de Projetos</h1>
+            <ul>
+                <a href="./rifa.php">
+                    <li>Rifa Online</li>
+                </a>
+                <a href="./calculadoraimc.php">
+                    <li>Calculadora IMC</li>
+                </a>
+                <a href="./conversormonetario.php">
+                    <li>Conversor de moedas</li>
+                </a>
+                <a href="./gorjetacalculator.php">
+                    <li>Calculadora de gorjetas</li>
+                </a>
+                <a href="./tempconverter.php">
+                    <li>Conversor de temperatura</li>
+                </a>
+                <a href="./calculadoraarea.php">
+                    <li>Calculadora de áreas</li>
+                </a>
+            </ul>
+        </main>
+        <footer>
+            <p>Desenvolvido por Kauã Valim - 2024</p>
+        </footer>
+    </body>'
+?>
 
 </html>
